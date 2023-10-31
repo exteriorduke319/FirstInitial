ce9a64abd0c6d110fa5de56c9782d107f0d8b509
exteriorduke319 <yarnevictanner@gmail.com>
2023-10-31 09:41:19 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(int i = 1; i <= 2; i++) {
+            System.out.println("******************");
+        }
+        for(int i = 0; i <= 6; i++) {
+            System.out.println("      ******      ");
+        }
     }
 }
